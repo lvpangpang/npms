417521c289f4546835e307de52755d90b0a08cb8
lvpangpang <574818075@qq.com>
2020-06-26 17:04:12 +0800

    http-server-bsv-代理

--- http-server-bsv/bin/http-server-bsv.js
+++ http-server-bsv/bin/http-server-bsv.js
@@ -3,13 +3,61 @@ const commander = require('commander');
 
 const { version } = require('../package.json');
 const Server = require('../src/index.js');
+const argv   = require('minimist')(process.argv.slice(2));
+
+if (argv.h || argv.help) {
+  console.log([
+    'usage: http-server [path] [options]',
+    '',
+    'options:',
+    '  -p --port    Port to use [8080]',
+    '  -a           Address to use [0.0.0.0]',
+    '  -d           Show directory listings [true]',
+    '  -i           Display autoIndex [true]',
+    '  -g --gzip    Serve gzip files when possible [false]',
+    '  -b --brotli  Serve brotli files when possible [false]',
+    '               If both brotli and gzip are enabled, brotli takes precedence',
+    '  -e --ext     Default file extension if none supplied [none]',
+    '  -s --silent  Suppress log messages from output',
+    '  --cors[=headers]   Enable CORS via the "Access-Control-Allow-Origin" header',
+    '                     Optionally provide CORS headers list separated by commas',
+    '  -o [path]    Open browser window after starting the server.',
+    '               Optionally provide a URL path to open the browser window to.',
+    '  -c           Cache time (max-age) in seconds [3600], e.g. -c10 for 10 seconds.',
+    '               To disable caching, use -c-1.',
+    '  -t           Connections timeout in seconds [120], e.g. -t60 for 1 minute.',
+    '               To disable timeout, use -t0',
+    '  -U --utc     Use UTC time format in log messages.',
+    '  --log-ip     Enable logging of the client\'s IP address',
+    '',
+    '  -P --proxy         Fallback proxy if the request cannot be resolved. e.g.: http://someurl.com',
+    '',
+    '  --username   Username for basic authentication [none]',
+    '               Can also be specified with the env variable NODE_HTTP_SERVER_USERNAME',
+    '  --password   Password for basic authentication [none]',
+    '               Can also be specified with the env variable NODE_HTTP_SERVER_PASSWORD',
+    '',
+    '  -S --ssl     Enable https.',
+    '  -C --cert    Path to ssl cert file (default: cert.pem).',
+    '  -K --key     Path to ssl key file (default: key.pem).',
+    '',
+    '  -r --robots        Respond to /robots.txt [User-agent: *\\nDisallow: /]',
+    '  --no-dotfiles      Do not show dotfiles',
+    '  -h --help          Print this list and exit.',
+    '  -v --version       Print the version and exit.'
+  ].join('\n'));
+  process.exit();
+}
+
+if(argv.v || argv.version) {
+  console.log(version);
+  process.exit();
+}
 
 commander
-  .option('-p,--port <n>', 'config port') // 配置端口
+  .option('-p,--port [value]', 'config port') // 配置端口
   .option('-o,--host [value]', 'config hostname') // 配置主机名
-  .option('-d,--dir [value]', 'config directory') // 配置访问目录
   .option('-P,--proxyUrl [value]', 'config proxy') // 配置转发地址
-  .version(version, '-v,--version') // 展示版本
   .parse(process.argv);
 
 const server = new Server(commander);
--- http-server-bsv/src/index.js
+++ http-server-bsv/src/index.js
@@ -7,6 +7,8 @@ const chalk = require('chalk');
 const url = require('url');
 const pug = require('pug');
 const mime = require('mime');
+const proxy = require('proxy');
+const iconv = require('iconv-lite');
 
 const config = require('./config');
 const stat = util.promisify(fs.stat);
@@ -24,37 +26,57 @@ class Server {
 
   // 处理请求入口
   async handleRequest(req, res) {
-    const { dir } = this.config;
+    const { dir, proxyUrl } = this.config;
     let { pathname } = url.parse(req.url);
     pathname = decodeURIComponent(pathname);
     let p = path.join(dir, pathname);
     try {
       let statObj = await stat(p);
+      // 文件夹处理
       if(statObj.isDirectory()) {
         res.setHeader('Conent-Type', 'text/html;charset=utf8');
         let dirs = await readdir(p);
-        dirs = dirs.map((item) => ({
-          name: item,
-          href: path.join(pathname, item)
-        }));
+        dirs = dirs.map((item) => {
+          let statItem = fs.statSync(path.join(dir, pathname, item));
+          return ({
+            name: item,
+            size: !statItem.isDirectory() ? (statItem.size / 1024).toFixed(2) + 'kb' : '',
+            href: path.join(pathname, item)
+          });
+        });
         let str = pug.render(this.template, {
+          url: pathname,
           arr: dirs
         });
         res.end(str);
       } else {
-        this.sendFile(req, res, statObj, p);
+        // 文件处理
+        this.sendFile(res, p);
       }
-    } catch(e) {
-      this.sendError(req, res);
+    } catch(err) {
+      if(proxyUrl) {
+        this.proxy(req, res, proxyUrl);
+      } else {
+        this.sendError(res, err);
+      }
     }
   }
 
-  sendFile(req, res, statObj, p)  {
-    res.setHeader('Conetent-Type', mime.getType(p) + ';charset=utf8');
+  sendFile(res, p) {
+    res.setHeader('Conetent-Type', mime.getType(p) + '; charset=UTF-8')
     fs.createReadStream(p).pipe(res);
   }
 
-  sendError(req, res){
+  proxy(req, res, proxyUrl) {
+    delete req.headers.host;
+    const proxy = httpProxy.createProxyServer({});
+    proxy.web(req, res, {
+      target: proxyUrl
+    });
+  }
+
+  sendError(res, err) {
+    console.log(err);
     res.end(`Error`);
   }
 
